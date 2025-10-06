d660e5a79b13c5945431e5aa1ec7071ebb87d1fc
Tim <timmywilliams4665@gmail.com>
2025-10-06 13:55:03 -0700

    Challenge 3

--- index.jsx
+++ index.jsx
@@ -1,15 +1,38 @@
 // //Challenge One
 // import {createRoot} from "react-dom/client";
 // import Ch1 from "./Challenges/Ch1/Ch1App.jsx";
-
 // const root = createRoot(document.querySelector("#root"));
-
 // root.render(<Ch1/>);
 
-//Challenge Two
-import {createRoot} from "react-dom/client";
-import Ch2 from "./Challenges/Ch2/Ch2App.jsx";
+// //Challenge Two
+// import {createRoot} from "react-dom/client";
+// import Ch2 from "./Challenges/Ch2/Ch2App.jsx";
+// const root = createRoot(document.querySelector("#root"));
+// root.render(<Ch2/>);
 
-const root = createRoot(document.querySelector("#root"));
+//Challenge Three
+import ReactDOM from 'react-dom/client';
 
-root.render(<Ch2/>);
+function App() {
+  
+  function handleClick() {
+    console.log("I was clicked!")
+  }
+  
+  function handleMouseOver() {
+    console.log("I was hovered!")
+  }
+  
+  return (
+    <main className="container">
+      <img
+        src="https://picsum.photos/640/360"
+        alt="Placeholder image from Picsum"
+        onMouseOver={handleMouseOver}
+      />
+      <button onClick={handleClick}>Click me</button>
+    </main>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
